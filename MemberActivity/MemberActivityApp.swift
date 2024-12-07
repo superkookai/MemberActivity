d55666ab587180c8d51c1b2b72d77976c1d7ee72
Weerawut <weerawut@live.com>
2024-12-07 09:30:18 +0700

    Initial Commit

---
+++ MemberActivity/MemberActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemberActivityApp.swift
+//  MemberActivity
+//
+//  Created by Weerawut Chaiyasomboon on 7/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemberActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
